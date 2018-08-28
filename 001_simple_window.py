3fd5233830ef536cc29fdb4fbced384029271b85
mambo4 <logan.e.bender@gmail.com>
2018-08-27 21:11:53 -0500

    initial upload

---
+++ 001_simple_window.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+from qtpy.QtWidgets import QApplication, QWidget
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Logan')
+    w.show()
+    
+    sys.exit(app.exec_())
+
